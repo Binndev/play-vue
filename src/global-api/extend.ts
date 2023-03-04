baa0c514b1cc8c81ce609b3a3128772227ce395b
binndev <875528791@qq.com>
2023-03-04 10:43:54 +0800

    feat extend

--- src/global-api/extend.ts
+++ src/global-api/extend.ts
@@ -5,10 +5,12 @@ export function initExtend(Vue: GlobalApi) {
   Vue.extend = function (options) {
     const Super = this
     const Sub = function VueComponent(this: any, options: any) {
+      options = options || {}
       this._init(options)
     } as unknown as typeof Component
 
     Sub.prototype = Object.create(Super.prototype)
+    Sub.prototype.constructor = Sub
     Sub.options = options
 
     return Sub
--- src/global-api/index.ts
+++ src/global-api/index.ts
@@ -1,9 +1,11 @@
 import { GlobalApi } from 'types/global-api'
 import { nextTick } from 'utils'
+import { initExtend } from './extend'
 import { initMixin } from './mixin'
 
 export function initGlobalApi(Vue: GlobalApi) {
   Vue.options = {}
   Vue.nextTick = nextTick
   initMixin(Vue)
+  initExtend(Vue)
 }
